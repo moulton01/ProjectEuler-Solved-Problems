56893d7550919854b3152126976791fa5375f086
BenM <Benmoulton93@gmail.com>
2015-05-25 20:30:39 +0100

    added revised version of ProjectEuler Problem 3

--- ProjectEuler/main.cpp
+++ ProjectEuler/main.cpp
@@ -1,16 +1,21 @@
 #include <iostream>
+#include <vector>
+#include <math.h>
 
 using namespace std; 
 
 int SumOfMultiplesOfThreeAndFive();
 int SumOfEvenFibonacci(); 
+bool isPrime(long num);
+long long largestPrimeFactor(long long num);
 
 int main(){
 
 	bool exit = false; 
+	long long numToCheck = 600851475143;
 	int input; 
 	do{
-	cout << "1. Problem One: Sum of multiples of 3 and 5?\n2.Even Fibonacci Numbers?\n";
+	cout << "1. Problem One: Sum of multiples of 3 and 5?\n2.Even Fibonacci Numbers?\n3. Largest Prime Factor?\n";
 	cin >> input; 
 
 		switch (input){
@@ -20,12 +25,17 @@ int main(){
 		case 2:
 			cout << "The sum of all even fibonacci numbers with a value less than 4 million are : " << SumOfEvenFibonacci() << endl; 
 			break;
+		case 3:
+			cout << "The largest prime factor of " << numToCheck << " is: " << largestPrimeFactor(numToCheck) << endl;
+			break;
 		default:
 			cout << "invalid input";
 			break; 
 
 		}
 	} while (!exit);
+
+
 
 	system("pause");
 }
@@ -71,4 +81,30 @@ int SumOfEvenFibonacci()
 
 	} while (temp < 4000000);
 	return sum; 
+}
+long long largestPrimeFactor(long long num)
+{
+	//Revised version. 
+	//First version was brute force, checking all numbers below the max that % 0.
+	//I then checked whether the numbers were prime. Repeated this until max factor found. 
+	long long newNum = num; 
+	long long largestFact = 0; 
+
+	int counter = 2; 
+	while (counter * counter <= newNum)
+	{
+		if (newNum % counter == 0)
+		{
+			newNum = newNum / counter; 
+			largestFact = counter; 
+		}
+		else {
+			counter++; 
+		}
+	}
+	if (newNum > largestFact)
+	{
+		largestFact = newNum;
+	}
+	return largestFact;
 }
